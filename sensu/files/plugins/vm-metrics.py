67b670453f910266fed03a799bb8df87f4a718a4
tanwar4 <tariq.anwar89@gmail.com>
2017-08-02 19:35:35 +0000

    script to collect vm metric

---
+++ sensu/files/plugins/vm-metrics.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+import xml.etree.ElementTree as ET
+import datetime
+
+command = "virsh list"
+
+remote_output = subprocess.check_output(command.split(), stderr=subprocess.STDOUT)
+output = "\n".join(remote_output.split("\n")[2:])  #remove the top two lines from the output
+
+total_vcpu = 0
+
+for line in output.splitlines():
+    instance_name = line.split()[1]  # extract the instance name
+    
+    #
+    command_dumpxml = "virsh dumpxml "+str(instance_name)
+    
+    dumpxml_output = subprocess.check_output(command_dumpxml.split(), stderr=subprocess.STDOUT)
+    root = ET.fromstring(dumpxml_output)
+    
+    try:
+       vcpu = int(root.find('vcpu').text)
+       total_vcpu = total_vcpu + vcpu    
+    except:
+       print "Cant find vcpu for : "+ str(instance_name)
+    
+       
+now = datetime.datetime.now()
+now_epoch = int(now.strftime("%s"))
+
+# Convert the time stamp to nano seconds.
+now_epoch = now_epoch*1000000000
+
+FORMATTER = "vCPU_metrics,metric={}, Category={},value={},timedelta_sec=60 {} \n"
+print FORMATTER.format("vCPU","Usage",total_vcpu,now_epoch)
+
